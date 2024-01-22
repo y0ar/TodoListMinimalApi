70666eab13370316e5f9893853c739f90297baf4
y0ar <youssef-801@hotmail.com>
2024-01-22 20:51:16 +0100

    Added POST method

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -7,7 +7,24 @@ async function getTodos() {
         .catch(error => console.error('Error:', error));
     
     return todos;
- }
+}
+
+async function addTodoItem() {
+    await fetch(uri, {
+        method: "POST",
+        body: JSON.stringify({
+          name: document.getElementById("txtTodoName").value,
+          isComplete: false
+        }),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8"
+        }
+    })
+    .then(response => response.json())
+    .catch(error => console.error('Error:', error));
+
+    await fillTable();
+}
 
 async function fillTable() {
     let todos = await getTodos();
@@ -27,17 +44,16 @@ async function fillTable() {
 
 function getTodoHtml(id, name, isComplete) {
     let todoHtml = `
-        <div class="flex mb-4 items-center">
-            <input type="text" disabled="true" hidden="true" value="${id}"/>
-            <p class="w-full text-grey-darkest">${name}</p>`
+        <div id="${id}" class="flex mb-4 items-center">
+            <p id="pName${id}" class="w-full text-grey-darkest">${name}</p>`
     
     if (isComplete === true) {
-        todoHtml += `<button id="btnCheckTodo" class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green">Done</button>`
+        todoHtml += `<button id="btnCheckTodo${id}" class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white-300 text-green border-green hover:bg-green-300">Done</button>`
     }
     else {
-        todoHtml += `<button id="btnCheckTodo" class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-grey border-grey hover:bg-grey">Not Done</button>`
+        todoHtml += `<button id="btnCheckTodo${id}" class="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white-300 text-grey border-grey hover:bg-blue-300">Not Done</button>`
     }
-    todoHtml += `<button id="btnDeleteTodo" class="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white hover:bg-red">Remove</button>
+    todoHtml += `<button id="btnDeleteTodo${id}" class="flex-no-shrink p-2 ml-2 border-2 rounded text-red border-red hover:text-white-300 hover:bg-red-300">Remove</button>
         </div>
         `
     return todoHtml;
